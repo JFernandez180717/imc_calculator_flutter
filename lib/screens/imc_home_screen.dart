64d22695dd928235f60393e4435b9e6f493bf1c7
JFernandez180717 <cta.cv2014@gmail.com>
2025-06-23 17:05:56 -0500

    Fully developed application

---
+++ lib/screens/imc_home_screen.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+import 'package:imc_calculator/components/gender_selector.dart';
+import 'package:imc_calculator/components/height_selector.dart';
+import 'package:imc_calculator/components/number_selector.dart';
+import 'package:imc_calculator/core/app_colors.dart';
+import 'package:imc_calculator/core/text_styles.dart';
+import 'package:imc_calculator/screens/imc_result_screen.dart';
+
+class ImcHomeScreen extends StatefulWidget {
+  const ImcHomeScreen({super.key});
+
+  @override
+  State<ImcHomeScreen> createState() => _ImcHomeScreenState();
+}
+
+class _ImcHomeScreenState extends State<ImcHomeScreen> {
+  double selectedHeight = 160;
+  int selectedAge = 0;
+  int selectedWeight = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        GenderSelector(),
+        HeightSelector(
+          selectedHeight: selectedHeight,
+          onHeightChange: (newHeight) {
+            setState(() {
+              selectedHeight = newHeight;
+            });
+          },
+        ),
+        Padding(
+          padding: const EdgeInsets.all(16),
+          child: Row(
+            children: [
+              Expanded(
+                child: NumberSelector(
+                  title: "PESO",
+                  value: selectedWeight,
+                  onIncrement: () {
+                    setState(() {
+                      selectedWeight++;
+                    });
+                  },
+                  onDecrement: () {
+                    setState(() {
+                      selectedWeight > 0 ? selectedWeight-- : null;
+                    });
+                  },
+                ),
+              ),
+              SizedBox(width: 16),
+              Expanded(
+                child: NumberSelector(
+                  title: "EDAD",
+                  value: selectedAge,
+                  onIncrement: () {
+                    setState(() {
+                      selectedAge++;
+                    });
+                  },
+                  onDecrement: () {
+                    setState(() {
+                      selectedAge > 0 ? selectedAge-- : null;
+                    });
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+        Spacer(),
+        Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: SizedBox(
+            height: 60,
+            width: double.infinity,
+            child: ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => ImcResultScreen(height: selectedHeight, weight: selectedWeight,)),
+                );
+              },
+              style: ButtonStyle(
+                shape: WidgetStateProperty.all(
+                  RoundedRectangleBorder(
+                    borderRadius: BorderRadiusGeometry.circular(8),
+                  ),
+                ),
+                backgroundColor: WidgetStateProperty.all(AppColors.primary),
+              ),
+              child: Text("Calcular", style: TextStyles.bodyText),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
